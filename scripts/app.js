30e4c2c64f86a7badf374fd79125a2701115f1b2
atkinsl88 <liamatkins24@gmail.com>
2020-07-20 18:11:46 +0100

    game variables working

--- scripts/app.js
+++ scripts/app.js
@@ -22,28 +22,31 @@ function init() {
 
   // DOM Elements
   const grid = document.querySelector('.grid')
+  const start = document.querySelector('#loader-btn')
   const cells = []
 
   // Grid Values
   const width = 10
   const cellCount = width * width
-  let linkPosition = 94
-  let zeldaPosition = 2
-  let gannonPosition = 10
-  let robotPosition = 20
-  let extraLifePosition = 45
 
-  // Function 1 - Link's Position
+  // Game Variables
+  let linkPosition = 94
+  let zeldaPosition = 4
+  let enemyPosition = 0
+  let randomEnemy = getRandomEnemyName()
+
+  // Function - Link's Position
   function createGrid(linkPosition) {
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
       cells.push(cell)
+      cell.innerHTML = i
       grid.appendChild(cell)
     }
     cells[linkPosition].classList.add('link')
   }
 
-  // Function 2 - Link's Movement
+  // Function - Link's Movement
   function handleKeyUp(event) {
 
     cells[linkPosition].classList.remove('link')
@@ -69,31 +72,28 @@ function init() {
     cells[linkPosition].classList.add('link') 
   }
 
-  // Function 3 - Zelda's Position
+  // Function - Zelda's Position
   function zeldasPosition(zeldaPosition) {
     cells[zeldaPosition].classList.add('zelda')
   }
 
-  // Function 4 - Gannon's Position
-  function gannonsPosition(gannonPosition) {
-    cells[gannonPosition].classList.add('gannon')
+  // Function - Random Enememy
+  function getRandomEnemyName() {
+    const names = ['gannon', 'robot']
+    return names[Math.floor(Math.random() * name.length)]
   }
 
-  // Function 5 - Robot's Positon
-  function robotsPosition(robotPosition) {
-    cells[robotPosition].classList.add('robot')
+  // Function = Game Logic
+  function startGame() {
+    enemyPosition = Math.floor(Math.random() * 100)
+    randomEnemy = getRandomEnemyName()
+    cells[enemyPosition].classList.add(randomEnemy)
   }
 
-  // Function 6 - Extra Life Positon
-  function extraLifesPosition(extraLifePosition) {
-    cells[extraLifePosition].classList.add('bonus')
-  }
 
   createGrid(linkPosition)
+  start.addEventListener('click', startGame)
   zeldasPosition(zeldaPosition)
-  gannonsPosition(gannonPosition)
-  robotsPosition(robotPosition)
-  extraLifesPosition(extraLifePosition)
 
   document.addEventListener('keyup', handleKeyUp)
 
